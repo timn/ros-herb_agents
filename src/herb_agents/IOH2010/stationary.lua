b0acc3c4e9b2870036cfc6f949219a7cdc974236
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-09-27 20:44:29 -0400

    IOH2010 stationary: current final version

--- src/herb_agents/IOH2010/stationary.lua
+++ src/herb_agents/IOH2010/stationary.lua
@@ -45,20 +45,38 @@ local utils = require("herb_agents.utils")
 fsm:define_states{ export_to=_M,
    closure={doorbell=doorbell, envlock=envlock},
    {"START", JumpState},
+   {"FAILED", JumpState},
    {"RECOVER", JumpState},
    {"RECOVER_RELEASE", AgentSkillExecJumpState, skills={{"releaseenv"}, {"goinitial"}}, final_state="RECOVER", failure_state="RECOVER"},
    {"ANNOUNCE_GRAB", AgentSkillExecJumpState, final_state="GRAB", failure_state="GRAB",
       skills={{"say", text="Detecting and grabbing bottle."}}},
-   {"GRAB", AgentSkillExecJumpState, final_state="DETERMINE_SIDE", failure_state="GRAB",
+   {"GRAB", AgentSkillExecJumpState, final_state="DETERMINE_SIDE", failure_state="GRAB_TRYAGAIN",
       skills={{"grab_object"}}},
+   {"GRAB_TRYAGAIN", AgentSkillExecJumpState, final_state="GRAB_TRYAGAIN2", failure_state="FAILED_RELAX_LEFT",
+      skills={{"goinitial", side="left"}, {"say", text="Oops, grasping failed, trying again."}}},
+   {"GRAB_TRYAGAIN2", AgentSkillExecJumpState, final_state="GRAB", failure_state="FAILED_RELAX_LEFT",
+      skills={{"goinitial", side="right"}}},
    {"DETERMINE_SIDE", JumpState},
    {"TURN", AgentSkillExecJumpState, skills={{"turn", angle_rad=-math.pi/2.}},
-      final_state="PUT_RECYCLE", failure_state="RECOVER"},
-   {"PUT_RECYCLE", AgentSkillExecJumpState, final_state="GOINITIAL", failure_state="RECOVER",
+      final_state="PUT_RECYCLE", failure_state="GIVE"},
+   {"GIVE", AgentSkillExecJumpState, final_state="GOINITIAL", failure_state="GOINITIAL",
+      skills={{"give"}, {"say", text="Motion component disabled. Please take."}}},
+   {"PUT_RECYCLE", AgentSkillExecJumpState, final_state="GOINITIAL", failure_state="GIVE_RECYCLE",
     skills={{"put"}, {"say", text="Saving the world. One bottle at a time."}}},
-   {"GOINITIAL", AgentSkillExecJumpState, skills={{"goinitial"}}, final_state="TURN_BACK", failure_state="RECOVER"},
+   {"GIVE_RECYCLE", AgentSkillExecJumpState, final_state="GOINITIAL", failure_state="FAILED_RELAX_LEFT",
+      skills={{"give"}, {"say", text="Cannot reach the recycling bin. Please take."}}},
+   {"GOINITIAL", AgentSkillExecJumpState, skills={{"goinitial"}}, final_state="TURN_BACK", failure_state="FAILED_RELAX_LEFT"},
    {"TURN_BACK", AgentSkillExecJumpState, skills={{"turn", angle_rad=math.pi/2.}},
     final_state="START", failure_state="RECOVER"},
+   {"FAILED_RELAX_LEFT", AgentSkillExecJumpState,
+    skills={{"relax_arm", side="left"}, {"say", text="User assistance required. Please help."}},
+    final_state="FAILED_RELAX_RIGHT", failure_state="FAILED_RELAX_RIGHT"},
+   {"FAILED_RELAX_RIGHT", AgentSkillExecJumpState, skills={{"relax_arm", side="right"}},
+    final_state="FAILED", failure_state="FAILED"},
+   {"FAILED_GOINITIAL_LEFT", AgentSkillExecJumpState, skills={{"goinitial", side="left"}},
+    final_state="FAILED_GOINITIAL_RIGHT", failure_state="FAILED_GOINITIAL_RIGHT"},
+   {"FAILED_GOINITIAL_RIGHT", AgentSkillExecJumpState, skills={{"goinitial", side="right"}},
+    final_state="START", failure_state="START"},
 }
 
 fsm:add_transitions{
@@ -67,6 +85,7 @@ fsm:add_transitions{
    {"DETERMINE_SIDE", "RECOVER", timeout=20},
    {"RECOVER", "START", timeout=5},
    {"RECOVER", "RECOVER_RELEASE", "#envlock.messages > 0 and envlock.messages[1].values.data", precond_only=true},
+   {"FAILED", "FAILED_GOINITIAL_LEFT", "#doorbell.messages > 0"},
 }
 
 function START:init()
@@ -79,11 +98,12 @@ end
 function DETERMINE_SIDE:loop()
    if #grabbed.messages > 0 then
       local m = grabbed.messages[#grabbed.messages] -- only check most recent
+      print_debug("Comparing %s/%s to %s", m.values.left_object_id, m.values.right_object_id, OBJECT_PATTERN)
       if m.values.left_object_id:match(OBJECT_PATTERN) then
 	 self.fsm.vars.side = "left"
 	 self.fsm.vars.object_id = m.values.left_object_id
 	 self.fsm.vars.side_determined = true
-      elseif m.values.left_object_id:match(OBJECT_PATTERN) then
+      elseif m.values.right_object_id:match(OBJECT_PATTERN) then
 	 self.fsm.vars.side = "right"
 	 self.fsm.vars.object_id = m.values.right_object_id
 	 self.fsm.vars.side_determined = true
@@ -98,7 +118,7 @@ end
 function GOINITIAL:init()
    self.skills[1].args = {side=self.fsm.vars.side}
 end
---HANDOFF.init = GRAB.init
+GIVE.init = GRAB.init
 
 function PUT_RECYCLE:init()
    self.skills[1].args = {side=self.fsm.vars.side, object_id="recyclingbin2"}
