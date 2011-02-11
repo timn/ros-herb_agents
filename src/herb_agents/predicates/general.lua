ae4a546a5110d5cbb239fcdf26804155a68bc7da
Kyle Strabala <strabala@cmu.edu>
2011-02-11 10:52:54 -0500

    predicates/general.lua: changed doorbell to callbutton, added HRI_yes and HRI_no functions

--- src/herb_agents/predicates/general.lua
+++ src/herb_agents/predicates/general.lua
@@ -5,6 +5,7 @@
 --  Created: Fri Oct 22 17:28:32 2010
 --  License: BSD, cf. LICENSE file
 --  Copyright  2010  Tim Niemueller [www.niemueller.de]
+--             2011  Kyle Strabala <strabala@cmu.edu>
 --             2010  Carnegie Mellon University
 --             2010  Intel Labs Pittsburgh
 ------------------------------------------------------------------------
@@ -12,25 +13,46 @@
 local predlib = require("fawkes.predlib")
 local math=math
 
---- This module provides generic soccer predicates.
+--- This module provides generic predicates.
 -- @author Tim Niemueller
 module(..., predlib.module_init)
 
 name = "herb_general"
 depends_topics = {
-   {v="doorbell", name="/callbutton", type="std_msgs/Byte"}
+   {v="callbutton", name="/callbutton", type="std_msgs/Byte"}
 }
 
 -- Initialize as predicate library
 predlib.setup(...)
 
+local HRI_NO_NUM = 1
+local HRI_YES_NUM = 2
 local DOORBELL_START_NUM = 4
 
 function start_button()
-   if #doorbell.messages > 0 then
-      local m = doorbell.messages[#doorbell.messages]
+   if #callbutton.messages > 0 then
+      local m = callbutton.messages[#callbutton.messages]
       return m.values.data == DOORBELL_START_NUM
    end
 
    return false
 end
+
+function HRI_yes()
+   if #callbutton.messages > 0 then
+      local m = callbutton.messages[#callbutton.messages]
+      return m.values.data == HRI_YES_NUM
+   end
+
+   return false
+end
+
+function HRI_no()
+   if #callbutton.messages > 0 then
+      local m = callbutton.messages[#callbutton.messages]
+      return m.values.data == HRI_NO_NUM
+   end
+
+   return false
+end
+
